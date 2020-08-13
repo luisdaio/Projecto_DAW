299427e92629cd5881b409900f4c891f6a71b7d7
Luís Fernandes <luisdaiofernandes@gmail.com>
2020-08-13 12:41:45 +0100

    Initial solution setup.

---
+++ Shop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shop.WebUI.Startup))]
+namespace Shop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
